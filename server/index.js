3d1fc8595fe2108307b8498da9c60c0a844189b9
Robert Niemela <rvniemela@hotmail.com>
2022-06-03 11:24:32 -0700

    chore: add slash to production folder

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ app.use("/api/posts", posts);
 
 // Handle production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static(__dirname + "/public"));
+  // Static folder
+  app.use(express.static(__dirname + "/public/"));
 
   // Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
