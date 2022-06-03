cef269cbbba4ea86ff3f21ed239c709e2a798e62
Robert Niemela <rvniemela@hotmail.com>
2022-06-02 17:39:31 -0700

    feat: show posts, allow creating/deleting

---
+++ client/src/PostService.js
@@ -0,0 +1,36 @@
+// Handle all requests. Will reference this from the component
+import axios from "axios";
+
+const url = "http://localhost:5000/api/posts";
+
+class PostService {
+  // Get Posts
+  static getPosts() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        resolve(
+          data.map((post) => ({
+            ...post,
+            createdAt: new Date(post.createdAt),
+          }))
+        );
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  // Create Post
+  static insertPost(postText) {
+    return axios.post(url, { text: postText });
+  }
+
+  // Delete Post
+  static deletePost(id) {
+    return axios.delete(`${url}/${id}`);
+  }
+}
+
+export default PostService;
--- client/src/components/PostItem.vue
+++ client/src/components/PostItem.vue
@@ -1,6 +1,60 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import PostService from "../PostService";
+
+const posts = ref([]);
+const error = ref("");
+const text = ref("");
+
+onMounted(async () => {
+  try {
+    posts.value = await PostService.getPosts();
+  } catch (err) {
+    error.value = err.message;
+  }
+});
+
+const addPost = async () => {
+  try {
+    await PostService.insertPost(text.value);
+    text.value = "";
+    posts.value = await PostService.getPosts();
+  } catch (err) {
+    error.value = err.message;
+  }
+};
+
+const deletePost = async (id) => {
+  try {
+    await PostService.deletePost(id);
+    posts.value = await PostService.getPosts();
+  } catch (err) {
+    error.value = err.message;
+  }
+};
+</script>
+
 <template>
-  <div class="item">
-    <h1>PostItem</h1>
+  <div>
+    <h1>Latest Posts</h1>
+
+    <div>
+      <input type="text" v-model="text" placeholder="Create a post" />
+      <button @click="addPost">Add Post</button>
+    </div>
+
+    <hr />
+
+    <p v-if="error">{{ error }}</p>
+    <ul>
+      <li v-for="post in posts" :key="post._id">
+        {{
+          `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`
+        }}
+        <p>{{ post.text }}</p>
+        <button @click="deletePost(post._id)">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
 
